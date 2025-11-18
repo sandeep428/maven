e0833ceb272e3a1fbe20d7c315601c1adf1caa0e
Ubuntu <ubuntu@ip-172-31-25-94.ec2.internal>
2025-11-18 13:59:13 +0000

    Rename app.java to App.java to fix public class issue

---
+++ src/main/java/com/sarav/App.java
@@ -0,0 +1,12 @@
+package com.sarav;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from TomcatMavenApp!");
+    }
+    
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
